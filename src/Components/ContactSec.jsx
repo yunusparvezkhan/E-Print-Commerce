2c9694fb33af712f689c6e355aa9504e074c01fd
Yunus Parvez Khan <yunus12.yp@gmail.com>
2022-07-15 23:13:39 +0530

    Completed the Contact Section in Landing page

--- src/Components/ContactSec.jsx
+++ src/Components/ContactSec.jsx
@@ -5,7 +5,15 @@ import BgImg from "../Media/images/ContactSecBg.png";
 const ContactSec = () => {
     return (
         <div className='Con-Container'>
-            <img src={BgImg} />
+            <img src={BgImg} className="cs-img" />
+            <div className='cs-textbox'>
+                <h1 className='cs-text cs-text-a'>You Can</h1>
+                <h1 className='cs-text cs-text-b'>Also</h1>
+                <h1 className='cs-text cs-text-c'>Tell us</h1>
+                <h1 className='cs-text cs-text-d'>About your</h1>
+                <h1 className='cs-text cs-text-e'>needs</h1>
+            </div>
+            <button className='cs-contact-btn' >Contact</button>
         </div>
     )
 }
